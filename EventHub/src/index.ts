c284ec89fa4dbdf113ed933584539cb6358af1c8
wky <wky_0615@163.com>
2020-02-23 21:28:27 +0800

    Eventhub

---
+++ EventHub/src/index.ts
@@ -0,0 +1,22 @@
+class EventHub {
+    events = {};
+    on(eventName, fn) {
+        if (this.events[eventName] !== undefined) {
+            this.events[eventName].push(fn)
+        } else {
+            this.events[eventName] = []
+            this.events[eventName].push(fn)
+        }
+    }
+    emit(eventName) {
+        if (this.events[eventName] === undefined) return
+        this.events[eventName].forEach(fn => fn())
+    }
+    off(eventName, fn) {
+        if (this.events[eventName] === undefined) return
+        const index = this.events[eventName].indexOf(fn)
+        index !== -1 && this.events[eventName].splice(index, 1)
+    }
+}
+
+export default EventHub;
---
+++ EventHub/test/index.ts
@@ -0,0 +1,36 @@
+import EventHub from '../src/index';
+
+const fn1 = () => {
+    console.log('eventHub .on method')
+}
+const fn2 = () => {
+    console.log('eventHub .off method')
+}
+
+const testEventHub = () => {
+    const eventHub = new EventHub
+    console.assert(eventHub instanceof Object === true)
+}
+
+const testOnMethod = () => {
+    const eventHub = new EventHub
+    eventHub.on('on', fn1)
+}
+
+const testEmitMethod = () => {
+    const eventHub = new EventHub
+    eventHub.on('on', fn1)
+    eventHub.emit('on')
+}
+
+const testOffMethod = () => {
+    const eventHub = new EventHub
+    eventHub.on('on', fn2)
+    eventHub.off('off', fn2)
+    eventHub.emit('off')
+}
+
+testEventHub()
+testOnMethod()
+testEmitMethod()
+testOffMethod()
