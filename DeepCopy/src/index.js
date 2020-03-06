b3241218976e4f0f75a1e15a09beb8df94ebb1ca
wky <wky_0615@163.com>
2020-03-06 17:09:45 +0800

    Compelete DeepCopy

--- DeepCopy/src/index.js
+++ DeepCopy/src/index.js
@@ -1,22 +1,44 @@
+let cache = [];
 const deepCopy = (source) => {
     if (source instanceof Object) {
-        let result;
-        if (source instanceof Array) {
-            result = new Array();
-        } else if (source instanceof Function) {
-            result = function() {
-                return source.apply(this, arguments)
-            }
+        let cachedResult = findCache(source)
+        if (cachedResult) {
+            return cachedResult
         } else {
-            result = new Object()
+            let result;
+            if (source instanceof Array) {
+                result = new Array();
+            } else if (source instanceof Function) {
+                result = function() {
+                    return source.apply(this, arguments)
+                }
+            } else if (source instanceof RegExp) {
+                result = new RegExp(source.source, source.flags)
+            } else if (source instanceof Date) {
+                result = new Date(source)
+            } else {
+                result = new Object()
+            }
+            cache.push([source, result])
+            for(let key in source) {
+                if (source.hasOwnProperty(key)) {
+                    if (result[key]) return;
+                    result[key] = deepCopy(source[key])
+                }
+            }
+            return result;
         }
-        for(let key in source) {
-            if (result[key]) return;
-            result[key] = deepCopy(source[key])
-        }
-        return result;
     }
     return source;
 };
+
+function findCache(source) {
+    for(let i = 0; i < cache.length; i++) {
+        if (cache[i][0] === source) {
+            return cache[i][1]
+        }
+    }
+    return undefined;
+}
 
 module.exports = deepCopy
--- DeepCopy/test/index.js
+++ DeepCopy/test/index.js
@@ -69,5 +69,44 @@ describe('deepCopy', () => {
             assert(fn.x.y.z === fn2.x.y.z)
             assert(fn(1, 2) === fn2(1, 2))
         })
+        it('能拷贝正则', () => {
+            const reg = new RegExp('hi\\d+', 'gi');
+            reg.x = {y: {z: 1}}
+            const reg2 = deepCopy(reg)
+            assert(reg !== reg2)
+            assert(reg.x !== reg2.x)
+            assert(reg.x.y !== reg2.x.y)
+            assert(reg.x.y.z === reg2.x.y.z)
+            assert(reg.source === reg2.source)
+            assert(reg.flags === reg2.flags)
+        })
+        it('能拷贝日期', () => {
+            const date = new Date()
+            date.x = {y: {z: 1}}
+            const date2 = deepCopy(date)
+            assert(date !== date2)
+            assert(date.x !== date2.x)
+            assert(date.x.y !== date2.x.y)
+            assert(date.x.y.z === date2.x.y.z)
+            assert(date.getTime() === date2.getTime())
+        })
+        it('自动跳过原型', () => {
+            const obj = Object.create({name: 'a'})
+            obj.x = {y: {z: 1}}
+            const obj2 = deepCopy(obj)
+            assert(obj !== obj2)
+            assert(obj.x !== obj2.x)
+            assert(obj.x.y !== obj2.x.y)
+            assert(obj.x.y.z === obj2.x.y.z)
+            assert.isFalse('name' in obj2)
+        })
+        it('能拷贝环', () => {
+            const obj = {name: 'obj'}
+            obj.self = obj
+            const obj2 = deepCopy(obj)
+            assert(obj !== obj2)
+            assert(obj.name === obj.name)
+            assert(obj.self !== obj2.self)
+        })
     })
 })
